ce1962cd9ded05037c69613e47a5b7fea4d4a777
Martin Koistinen <mkoistinen@gmail.com>
2015-08-10 18:23:39 -0400

    Adds South migration for new model

--- cms/models/apphooks_reload.py
+++ cms/models/apphooks_reload.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals, absolute_import
+from __future__ import unicode_literals
 
 from django.db import models
 
 
 class UrlconfRevision(models.Model):
     revision = models.CharField(max_length=255)
+
+    class Meta:
+        app_label = 'cms'
